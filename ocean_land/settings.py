45e0e0d806d51b1927c2ce73dd75b41e2239ff8a
yunyu <1287641566@qq.com>
2021-11-16 23:45:15 +0800

    项目构建：配置数据库

--- ocean_land/settings.py
+++ ocean_land/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'ocean_land.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oceanland',
+        'USER': 'root',
+        'PASSWORD': 'li123456',
+        'HOST': '106.14.220.57',
+        'PORT': '3307'
     }
 }
 
