6858866749a8e496abf1512e604fdbe774881ff4
yunyu <1287641566@qq.com>
2021-11-16 23:51:32 +0800

    项目构建：添加drf-yasg来生成swagger文档

--- ocean_land/urls.py
+++ ocean_land/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(title="接口测试文档",  # 必传
+                 default_version='v1',  # 必传
+                 description="测试平台接口文档",
+                 terms_of_service="https://www.google.com/policies/terms/",
+                 contact=openapi.Contact(email="1287641566@qq.com"),
+                 license=openapi.License(name="BSD License"),
+                 ),
+    public=True,
+    # permission_classes=(permissions.AllowAny,), #权限类
+
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
