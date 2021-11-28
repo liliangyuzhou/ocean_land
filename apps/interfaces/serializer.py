cea134b2c25d171f5db28d5115921e69b67779e0
yunyu <1287641566@qq.com>
2021-11-28 22:58:17 +0800

    项目构建：接口子应用添加序列器文件

---
+++ apps/interfaces/serializer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： liliang
+# datetime： 2021/11/18 09:54 
+# email： liang1.li@ximalaya.com
+from rest_framework import serializers
+from .models import Interfaces
+
+
+
