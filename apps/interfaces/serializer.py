0ede43d91913d6888ae9a9a2c607f0273db974d2
yunyu <1287641566@qq.com>
2021-11-30 00:15:54 +0800

    项目构建：修改接口子应用，接口创建，更新，以及获取接口的全部测试用例，接口的全部配置

--- apps/interfaces/serializer.py
+++ apps/interfaces/serializer.py
@@ -5,9 +5,14 @@
 # email： liang1.li@ximalaya.com
 from rest_framework import serializers
 from .models import Interfaces
+from projects.models import Projects
 
 class InterfacesSerializer(serializers.ModelSerializer):
-    # project=serializers.StringRelatedField(read_only=True,label="所属项目名称")
+    #StringRelatedField默认是read_only=True,可以查看源码
+    project=serializers.StringRelatedField(label="所属项目名称",help_text="所属项目名称")
+    #queryset=Projects.objects.all()，对前端输入的project_id进行了一个校验，传入的项目id必须存在
+    #PrimaryKeyRelatedField, 前端传的是id，这个字段会自动转换为Projects模型类对象，可以断点发现
+    project_id=serializers.PrimaryKeyRelatedField(queryset=Projects.objects.all(),help_text="项目id")
     class Meta:
         model=Interfaces
         exclude=('update_time','is_delete')
@@ -16,6 +21,16 @@ class InterfacesSerializer(serializers.ModelSerializer):
                 'read_only':True,
             }
         }
+    def create(self, validated_data):
+        project=validated_data.pop('project_id')
+        validated_data['project']=project
+        interface=Interfaces.objects.create(**validated_data)
+        return interface
+    def update(self, instance, validated_data):
+        if 'project_id' in validated_data:
+            project = validated_data.pop('project_id')
+            validated_data['project'] = project
+        return super(InterfacesSerializer, self).update(instance,validated_data)
 
 class InterfacesNameSerializer(serializers.ModelSerializer):
     class Meta:
--- apps/interfaces/views.py
+++ apps/interfaces/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import decorators
 from .serializer import InterfacesSerializer,InterfacesNameSerializer
 from .models import Interfaces
+from apps.testcases.models import Testcases
+from configures.models import Configures
 from utils.tools import get_count_by_interface
 # Create your views here.
 
@@ -43,7 +45,29 @@ class InterfacesViewSet(viewsets.ModelViewSet):
         else:
             return self.serializer_class
 
+    @decorators.action(detail=True)
+    def testcases(self,request,pk=None):
+        testcases=Testcases.objects.filter(interface_id=pk,is_delete=False)
+        one_list=[]
+        for item in testcases:
+            one_list.append(
+                {'id':item['id'],
+                 'name':item['name']
+                 }
+            )
+        return Response(data=one_list)
 
+    @decorators.action(detail=True,url_path='configs')
+    def configures(self,request,pk=None):
+        configures=Configures.objects.filter(interface_id=pk,is_delete=False)
+        one_list=[]
+        for item in configures:
+            one_list.append(
+                {'id':item['id'],
+                 'name':item['name']
+                 }
+            )
+        return Response(data=one_list)
 
 
 
