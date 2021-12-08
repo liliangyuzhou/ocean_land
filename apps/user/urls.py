e3448d7f0e0aca1ba3283924520c448936ff881f
yunyu <1287641566@qq.com>
2021-12-08 13:52:46 +0800

    项目修复：增加用户名是否使用和邮箱是否使用

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,7 +4,7 @@
 # datetime： 2021/11/17 11:29 
 # email： liang1.li@ximalaya.com
 
-from django.urls import path
+from django.urls import path,re_path
 from rest_framework_jwt.views import obtain_jwt_token, ObtainJSONWebToken
 from apps.user import views
 urlpatterns = [
@@ -12,4 +12,9 @@ urlpatterns = [
     # 在子应用中添加jwt认证的login接口，path('login/', ObtainJSONWebToken.as_view())和下面一样
     path('login/', obtain_jwt_token),
     path('register/',views.RegisterView.as_view()),
+    #验证用户名是否存在
+    re_path(r'^(?P<username>\w{6,20})/count/$', views.UsernameIsExistedView.as_view()),
+    #验证邮件是否存在
+    re_path(r'^(?P<email>[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+)/count/$',
+            views.EmailIsExistedView.as_view()),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics
+from rest_framework import generics,response
 from rest_framework.views import APIView
 from .serializer import RegisterModelSerializer
 from rest_framework import permissions
+from django.contrib.auth.models import User
 
 #写类视图要继承什么类，看看需求需要那些方法
 #注册不要要开启权限
@@ -18,3 +19,27 @@ class RegisterView(generics.CreateAPIView):
     permission_classes =(permissions.AllowAny,)
     #类视图中使用jwt token进行认证
     # authentication_classes =('JSONWebTokenAuthentication',)
+
+
+class UsernameIsExistedView(APIView):
+
+    def get(self, request, username):
+        count = User.objects.filter(username=username).count()
+        one_dict = {
+            'username': username,
+            'count': count
+        }
+
+        return response.Response(one_dict)
+
+
+class EmailIsExistedView(APIView):
+
+    def get(self, request, email):
+        count = User.objects.filter(email=email).count()
+        one_dict = {
+            'email': email,
+            'count': count
+        }
+
+        return response.Response(one_dict)
